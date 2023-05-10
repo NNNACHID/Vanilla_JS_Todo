70a80c15309fca1b9d51c5d5e9a265e09a20b676
trinumnnachid <n.nachid@trinum.com>
2023-05-10 14:56:25 +0200

    feat: :construction: Some JS, button etc

---
+++ app.js
@@ -0,0 +1,35 @@
+// SELECTORS
+const todoInput = document.querySelector(".todo__input");
+const todoButton = document.querySelector(".todo__button");
+const todoList = document.querySelector(".todo__list");
+
+// LISTENERS
+todoButton.addEventListener("click", addTodo);
+
+// FUNCTIONS 
+function addTodo(event){
+
+    event.preventDefault();
+
+    // Todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    // Create the <li>
+    const newTodo = document.createElement("li");
+    newTodo.innerText = "Hey";
+    newTodo.classList.add("todo__item");
+    todoDiv.appendChild(newTodo);
+
+    //Check button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete__btn");
+    todoDiv.appendChild(completedButton);
+
+    //Delete button
+    const deleteButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-trash"></i>';
+    completedButton.classList.add("delete__btn");
+    todoDiv.appendChild(deleteButton);
+}
